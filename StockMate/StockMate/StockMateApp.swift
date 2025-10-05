cbf6139ab2abccd6904a61075546ffacb290f840
유현아 <yha201341@gmail.com>
2025-10-05 10:24:11 +0900

    Initial commit: Xcode project setup

---
+++ StockMate/StockMate/StockMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMateApp.swift
+//  StockMate
+//
+//  Created by Admin on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct StockMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
