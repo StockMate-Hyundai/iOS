b5325df450cc05250ebf6b7cd13bb6b9a160c4cc
유현아 <yha201341@gmail.com>
2025-10-19 13:46:49 +0900

    Revert "[FIX] 재고검색 페이지 버그 수정"
    This reverts commit ad06779953a1d6caecf4e5e0a51b937215ae9944.

--- StockMate/StockMate/app/feature/inventory/ui/InventorySearchView.swift
+++ StockMate/StockMate/app/feature/inventory/ui/InventorySearchView.swift
@@ -87,15 +87,23 @@ struct InventorySearchView: View {
 
                     TextField("부품을 검색하세요.", text: $searchText)
                         .textFieldStyle(PlainTextFieldStyle())
+//                        .onChange(of: searchText) { newValue in
+//                            inventoryViewModel
+//                                .searchInFilteredList(keyword: newValue)
+//                        }
                         .onChange(of: searchText) { newValue in
-                            inventoryViewModel
-                                .searchInFilteredList(keyword: newValue)
-                        }
+                           let term = newValue.trimmingCharacters(in: .whitespacesAndNewlines)
+                           if term.isEmpty {
+                               Task { await inventoryViewModel.loadInventoryList(reset: true) }
+                           } else {
+                               Task { await inventoryViewModel.searchByName(name: term, reset: true) }
+                           }
+                       }
                     
                     if !searchText.isEmpty {
                         Button(action: {
                             searchText = ""
-                            inventoryViewModel.isSearching = false
+                            //inventoryViewModel.isSearching = false
                         }) {
                             Image(systemName: "xmark")
                                 .foregroundColor(.gray)
@@ -169,10 +177,7 @@ struct InventorySearchView: View {
                                             Task {
                                                 await inventoryViewModel
                                                     .searchByName(
-                                                        name: searchText
-                                                            .trimmingCharacters(
-                                                                in: .whitespacesAndNewlines
-                                                            )
+                                                        name: searchText.trimmingCharacters(in: .whitespacesAndNewlines)
                                                     )
                                             }
                                         }
