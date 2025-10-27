96e82275dfc7c75b67df893ce716943bfc3d66a0
유현아 <yha201341@gmail.com>
2025-10-27 12:34:29 +0900

    [FEAT] 직접발주 구현

--- StockMate/StockMate/app/feature/orders/ui/OrderView.swift
+++ StockMate/StockMate/app/feature/orders/ui/OrderView.swift
@@ -8,120 +8,126 @@
 import SwiftUI
 
 struct OrderView: View {
-    @StateObject private var inventoryViewModel = InventoryViewModel()
-    @StateObject private var cartViewModel = CartViewModel()
-
+    @StateObject var inventoryViewModel = InventoryViewModel()
+    @ObservedObject var cartViewModel: CartViewModel
+    
     var body: some View {
-        NavigationStack {
-            ZStack{
+        ZStack{
+            ScrollView {
+                // 타이틀
+                Text("재고 관리")
+                    .font(.title2)
+                    .bold()
+                    .padding(.top, 13)
+                    .padding(.leading, 25)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 
-                ScrollView {
-                    // 타이틀
-                    Text("재고 관리")
-                        .font(.title2)
-                        .bold()
-                        .padding(.top, 13)
-                        .padding(.leading, 25)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Text("직접 발주")
-                        .font(.system(size: 14))
-                        .bold()
-                        .padding(.top, 13)
-                        .padding(.leading, 25)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    // 🔍 검색창
-                    NavigationLink(destination: InventorySearchView()) {
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(.gray)
-                            
-                            Text("부품을 검색하세요.")
-                                .foregroundColor(.gray)
-                            Spacer()
-                        }
-                        .padding()
-                        .background(Color(.white))
-                        .cornerRadius(9999)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 9999)
-                                .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                        )
-                        .padding(.horizontal)
+                Text("직접 발주")
+                    .font(.system(size: 14))
+                    .bold()
+                    .padding(.top, 13)
+                    .padding(.leading, 25)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                // 🔍 검색창
+                NavigationLink(destination:
+                                OrderRequestSearchView(
+                                    cartViewModel: cartViewModel
+                                    //inventoryViewModel: inventoryViewModel
+                                )
+                ) {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        Text("부품을 검색하세요.")
+                            .foregroundColor(.gray)
+                        Spacer()
                     }
-                    .buttonStyle(.plain)
-                    
-                    // 타이틀
-                    Text("부족 재고")
-                        .font(.system(size: 14))
-                        .bold()
-                        .padding(.top, 13)
-                        .padding(.leading, 25)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    LazyVStack(alignment: .leading, spacing: 14) {
-                        ForEach(inventoryViewModel.underLimitItems) { item in
-                            
-                            let qty = cartViewModel.quantity(for: item.id)
-                            
-                            OrderRequestCardView(
-                                item: item,
-                                quantity: qty,
-                                onIncrease: {
-                                    Task {
-                                        await cartViewModel.increaseQuantity(for: item.id)
-                                    }
-                                },
-                                onDecrease: {
-                                    Task {
-                                        await cartViewModel.decreaseQuantity(for: item.id)
-                                    }
-                                },
-                                onAddToCart: {
-                                    Task {
-                                        await cartViewModel.addToCart(partId: item.id, amount: 1)
-                                    }
-                                },
-                                onRemoveFromCart: {
-                                    Task {
-                                        await cartViewModel.decreaseQuantity(for: item.id)
-                                    }
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(9999)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 9999)
+                            .stroke(Color.gray.opacity(0.4), lineWidth: 1)
+                    )
+                    .padding(.horizontal)
+                }
+                .buttonStyle(.plain)
+                
+                // 타이틀
+                Text("부족 재고")
+                    .font(.system(size: 14))
+                    .bold()
+                    .padding(.top, 13)
+                    .padding(.leading, 25)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                LazyVStack(alignment: .leading, spacing: 14) {
+                    ForEach(inventoryViewModel.underLimitItems) { item in
+                        
+                        let qty = cartViewModel.quantity(for: item.id)
+                        
+                        OrderRequestCardView(
+                            item: item,
+                            quantity: qty,
+                            onIncrease: {
+                                Task {
+                                    await cartViewModel.increaseQuantity(for: item.id)
                                 }
-                            )
-                            .onAppear {
-                                if item.id == inventoryViewModel.underLimitItems.last?.id {
-                                    Task { await inventoryViewModel.loadUnderLimitList() }
+                            },
+                            onDecrease: {
+                                Task {
+                                    await cartViewModel.decreaseQuantity(for: item.id)
+                                }
+                            },
+                            onAddToCart: {
+                                Task {
+                                    await cartViewModel.addToCart(partId: item.id, amount: 1)
+                                }
+                            },
+                            onRemoveFromCart: {
+                                Task {
+                                    await cartViewModel.decreaseQuantity(for: item.id)
                                 }
                             }
-                        }
-                        
-                        if inventoryViewModel.isLoading {
-                            ProgressView().padding()
+                        )
+                        .onAppear {
+                            if item.id == inventoryViewModel.underLimitItems.last?.id {
+                                Task { await inventoryViewModel.loadUnderLimitList() }
+                            }
                         }
                     }
-                    .padding(.horizontal)
                     
+                    if inventoryViewModel.isLoading {
+                        ProgressView().padding()
+                    }
                 }
-                .background(Color.Light)
-                .task {
-                    await inventoryViewModel.loadUnderLimitList(reset: true)
-                    await cartViewModel.fetchCart()
-                }
+                .padding(.horizontal)
                 
-                // OrderView 내부 ScrollView 아래 장바구니 확인 버튼
-                VStack {
-                    Spacer()
-                    CartSummaryBar(cartVM: cartViewModel)
-                }
-                .ignoresSafeArea(edges: .bottom)
-
             }
+            .background(Color.Light)
+            .task {
+                if inventoryViewModel.underLimitItems.isEmpty {
+                       await inventoryViewModel.loadUnderLimitList(reset: true)
+                   }
+//                await inventoryViewModel.loadUnderLimitList(reset: true)
+                await cartViewModel.fetchCart()
+            }
+            
+            // OrderView 내부 ScrollView 아래 장바구니 확인 버튼
+            VStack {
+                Spacer()
+                CartSummaryBar(cartVM: cartViewModel)
+            }
+            .ignoresSafeArea(edges: .bottom)
+
         }
+
     }
 }
 
-
-#Preview {
-    OrderView()
-}
+//
+//#Preview {
+//    OrderView()
+//}
--- StockMate/StockMate/app/navigation/MainTabView.swift
+++ StockMate/StockMate/app/navigation/MainTabView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @StateObject var cartVM = CartViewModel()
+    
     @State private var selectedTab = 0
     @State private var tabTappedTrigger = false
     
@@ -17,7 +19,8 @@ struct MainTabView: View {
             ZStack {
                 switch selectedTab {
                 case 0: NavigationStack{ HomeView() }
-                case 1: NavigationStack{ OrderView() }
+                case 1: NavigationStack{ OrderView(cartViewModel: cartVM) } //, inventoryViewModel: inventoryVM) }
+//                case 1: NavigationStack{ OrderView() }
                 case 2:
                 NavigationStack { InventoryView(selectedTab: $selectedTab, tabTappedTrigger: $tabTappedTrigger) }
 //                case 3: NavigationStack{ ContentView() }
